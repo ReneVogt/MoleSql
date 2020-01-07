5ce6ac87527ffcb5317f5e64492c8ecb0ed63df7
René Vogt <rvogt@aki-gmbh.de>
2020-01-07 09:18:28 +0100

    Unit-Tests for ToListAsync

---
+++ Sources/MoleSqlTests/TestToListAsync.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * (C)2020 by René Vogt
+ *
+ * Published under MIT license as described in the LICENSE.md file.
+ *
+ */
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoleSql.QueryProviders;
+
+namespace MoleSqlTests
+{
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class TestToListAsync
+    {
+        [TestMethod]
+        [ExpectedException(typeof(NotSupportedException))]
+        public async Task ToListAsync_WrongQueryProvider_Exception()
+        {
+            await new object[0].AsQueryable().ToListAsync();
+        }
+        [TestMethod]
+        public async Task ToListAsync_CorrectList()
+        {
+            using var context = MoleSqlTestContext.GetDbContext();
+            var list = await context.Employees.OrderBy(e => e.Name).Select(e => e.Name).ToListAsync();
+            list.Should().Equal("Marc", "Marcel", "René", "Steve");
+        }
+    }
+}
