b00c2319403effe9f00b0ad1db311c1c4525ef8f
René Vogt <rvogt@aki-gmbh.de>
2020-01-08 14:25:25 +0100

    Fixed nested queries (Evaluator nominated NewExpression inside a MemberInitExpression)

--- Sources/MoleSql/Translators/LocalEvaluator.cs
+++ Sources/MoleSql/Translators/LocalEvaluator.cs
@@ -25,6 +25,8 @@ namespace MoleSql.Translators
             readonly HashSet<Expression> candidates = new HashSet<Expression>();
             readonly Func<Expression, bool> isEvaluatable;
             bool canBeEvaluated = true;
+            bool nestedCantBeEvaluated;
+            int nestedCompoundExpression;
 
             Nominator(Func<Expression, bool> isEvaluatable)
             {
@@ -39,10 +41,22 @@ namespace MoleSql.Translators
                 canBeEvaluated = true;
                 base.Visit(node);
                 canBeEvaluated &= isEvaluatable(node);
-                if (canBeEvaluated) candidates.Add(node);
+                if (canBeEvaluated && nestedCompoundExpression == 0) candidates.Add(node);
+                if (!canBeEvaluated) nestedCantBeEvaluated = true;
                 canBeEvaluated &= oldCanBeEvaluated;
                 return node;
             }
+            protected override Expression VisitMemberInit(MemberInitExpression node)
+            {
+                nestedCantBeEvaluated = false;
+                nestedCompoundExpression += 1;
+                var result = base.VisitMemberInit(node);
+                if (nestedCantBeEvaluated) canBeEvaluated = false;
+                nestedCompoundExpression -= 1;
+                if (nestedCompoundExpression == 0 && canBeEvaluated) candidates.Add(node);
+                return result;
+            }
+
             internal static HashSet<Expression> Nominate(Expression expression, Func<Expression, bool> canBeEvaluated)
             {
                 var nominator = new Nominator(canBeEvaluated);
@@ -61,7 +75,6 @@ namespace MoleSql.Translators
 
             public override Expression Visit(Expression node) => node == null ? null : candidates.Contains(node) ? Evaluate(node) : base.Visit(node);
 
-            internal static Expression Evaluate(Expression expression, HashSet<Expression> nominees) => new Evaluator(nominees).Visit(expression);
             static Expression Evaluate(Expression expression)
             {
                 if (expression.NodeType == ExpressionType.Constant)
@@ -71,6 +84,8 @@ namespace MoleSql.Translators
                 var function = lambda.Compile();
                 return Expression.Constant(function.DynamicInvoke(null), expression.Type);
             }
+
+            internal static Expression Evaluate(Expression expression, HashSet<Expression> nominees) => new Evaluator(nominees).Visit(expression);
         }
         /// <summary>
         /// Traverses the given <paramref name="expression"/> and evaluates all parts of it that can be
--- Sources/MoleSqlTests/TestNestedQueries.cs
+++ Sources/MoleSqlTests/TestNestedQueries.cs
@@ -22,14 +22,25 @@ namespace MoleSqlTests
             using var context = MoleSqlTestContext.GetDbContext();
             var query = from employee in context.Employees
                         where employee.Name == "René"
-                        select new {employee.Name, Orders = context.Orders.Where(order => order.EmployeeId == employee.Id).OrderBy(order => order.Id)};
+                        select new
+                        {
+                            employee.Name, Orders = context.Orders.Where(order => order.EmployeeId == employee.Id).OrderBy(order => order.Id)
+                        };
             var result = query.AsEnumerable().Select(x => new {x.Name, Orders = x.Orders.AsEnumerable().ToList()}).ToList();
 
             result.Should().HaveCount(1);
             result[0].Name.Should().Be("René");
             result[0].Orders.Should().HaveCountGreaterThan(0);
             result[0].Orders[0].Id.Should().Be(1);
-            MoleSqlTestContext.AssertSqlDump(context, "DELETE FROM Customers WHERE [Name] = @p0 -- @p0 NVarChar Input [Alfons Allerlei]");
+            MoleSqlTestContext.AssertSqlDump(context, @"
+SELECT [t0].[Name], [t0].[Id] 
+FROM [Employees] AS t0 WHERE ([t0].[Name] = @p0) 
+-- @p0 NVarChar Input [René] 
+            
+SELECT [t3].[Id], [t3].[CustomerId], [t3].[EmployeeId], [t3].[Date], [t3].[Discount] 
+FROM [Orders] AS t3 WHERE ([t3].[EmployeeId] = @p0) 
+ORDER BY [t3].[Id] 
+-- @p0 Int Input [5]");
         }
     }
 }
