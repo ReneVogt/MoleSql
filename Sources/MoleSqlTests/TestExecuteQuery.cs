ff4a5066ffc05e283843da4b41f241fd36171a00
René Vogt <rvogt@aki-gmbh.de>
2020-01-08 16:16:21 +0100

    unit-tests

--- Sources/MoleSqlTests/TestExecuteQuery.cs
+++ Sources/MoleSqlTests/TestExecuteQuery.cs
@@ -7,6 +7,7 @@
 
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
@@ -18,15 +19,49 @@ namespace MoleSqlTests
     [ExcludeFromCodeCoverage]
     public class TestExecuteQuery
     {
+        sealed class TestRow
+        {
+            // ReSharper disable UnusedAutoPropertyAccessor.Local
+            public int Identifier { get; set; }
+            public string Description { get; set; }
+            // ReSharper restore UnusedAutoPropertyAccessor.Local
+        }
+
+        [TestMethod]
+        public void ExecuteQuery_Generic_CorrectResults()
+        {
+            using var context = MoleSqlTestContext.GetDbContext();
+            var query = context.ExecuteQuery<TestRow>($"SELECT TOP 4 [Id] AS Identifier, [Name] AS Description FROM Departments ORDER BY [Id]");
+            query.Select(x => (x.Identifier, x.Description)).Should().Equal((1, "Marketing"), (2, "Sales"), (3, "Support"), (4, "Development"));
+        }
+        [TestMethod]
+        public async Task ExecuteQueryAsync_Generic_CorrectResults()
+        {
+            using var context = MoleSqlTestContext.GetDbContext();
+            var query = context.ExecuteQueryAsync<TestRow>($"SELECT TOP 4 [Id] AS Identifier, [Name] AS Description FROM Departments ORDER BY [Id]", CancellationToken.None);
+
+            var resultList = new List<(int, string)>();
+            await foreach (var element in query)
+                resultList.Add((element.Identifier, element.Description));
+
+            resultList.Should().Equal((1, "Marketing"), (2, "Sales"), (3, "Support"), (4, "Development"));
+        }
+        [TestMethod]
+        public void ExecuteQuery_Dynamic_CorrectResults()
+        {
+            using var context = MoleSqlTestContext.GetDbContext();
+            var query = context.ExecuteQuery($"SELECT TOP 4 [Id] AS Identifier, [Name] AS Description FROM Departments ORDER BY [Id]");
+            query.Cast<dynamic>().Select(x => (x.Identifier, x.Description)).Should().Equal((1, "Marketing"), (2, "Sales"), (3, "Support"), (4, "Development"));
+        }
         [TestMethod]
         public async Task ExecuteQueryAsync_Dynamic_CorrectResults()
         {
             using var context = MoleSqlTestContext.GetDbContext();
-            var query = context.ExecuteQueryAsync($"SELECT TOP 4 [Id] AS Erkennungszahl, [Name] AS Bezeichnung FROM Departments ORDER BY [Id]", CancellationToken.None);
+            var query = context.ExecuteQueryAsync($"SELECT TOP 4 [Id] AS Identifier, [Name] AS Description FROM Departments ORDER BY [Id]", CancellationToken.None);
 
             var resultList = new List<(int, string)>();
             await foreach (var element in query)
-                resultList.Add((element.Erkennungszahl, element.Bezeichnung));
+                resultList.Add((element.Identifier, element.Description));
 
             resultList.Should().Equal((1, "Marketing"), (2, "Sales"), (3, "Support"), (4, "Development"));
         }
